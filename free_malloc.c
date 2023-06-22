0da9d3ad60e82338a393781440574f7bb1703dde
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-22 20:43:55 +0200

    fixed betty issues

--- free_malloc.c
+++ free_malloc.c
@@ -19,13 +19,14 @@ void _free_stack(stack_t *stack)
 
 /**
  * free_input_array - frees an Array holding the input from the file.
- * @buffer; the array or pointer to the array of strings.
+ * @buffer: the array or pointer to the array of strings.
  * @size: number of elements in a buffer.
  * Return: void.
  */
 void free_input_array(char **buffer, int size)
 {
 	int i;
+
 	for (i = 0; i < size; i++)
 		free(buffer[i]);
 	free(buffer);
