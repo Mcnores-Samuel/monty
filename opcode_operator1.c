39eaae2d38cd3ec60c8512472206597f8e624411
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-24 15:18:32 +0200

    subtracts two top elements of the stack

--- opcode_operator1.c
+++ opcode_operator1.c
@@ -36,3 +36,26 @@ int nop(stack_t **stack, unsigned int num)
 
 	return (1);
 }
+
+/**
+ * sub - subtracts the top two elements of the stack.
+ * @stack: double pointer to the head node of the stack.
+ * @num: line numbers from the instruction file.
+ * Return: 1 on success or -1 otherwise.
+ */
+int sub(stack_t **stack, unsigned int num)
+{
+	int n = num;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", num);
+		return (-1);
+	}
+
+	num = (*stack)->n - (*stack)->next->n;
+	pop(stack, n);
+	pop(stack, n);
+	push(stack, num);
+	return (1);
+}
