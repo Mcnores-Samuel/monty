d424bf0e4a6d881b3ea69e6bd7f9b327e99f366d
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-06-24 15:05:28 +0200

    dds the top two elements of the stack.

--- myfile.m
+++ myfile.m
@@ -1,7 +1,1 @@
-push 10
-push 30
-push 45
-push 55
-pall
 add
-pall
--- opcode_operator1.c
+++ opcode_operator1.c
@@ -13,6 +13,7 @@ int add(stack_t **stack, unsigned int num)
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", num);
+		return (-1);
 	}
 
 	num = (*stack)->n + (*stack)->next->n;
